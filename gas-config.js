381f123d6b0f091d346e6f80c1f7f684da3a62fb
tecmopia-kozunomori <tekumopiagongjinnodu@gmail.com>
2026-07-24 17:11:46 +0900

    Update gas-config.js

--- gas-config.js
+++ gas-config.js
@@ -1,2 +1,2 @@
 // TECMOPIA 利用状況ダッシュボード連携先
-window.TECMOPIA_GAS_URL = "https://script.google.com/macros/s/AKfycbw1cp2mkb8UQU9NAenlwBmqd9ozO9wvdliIMtNk2KdLHDyzAHanXO5L-MzJhBvGq2ga/exec";
+window.TECMOPIA_GAS_URL = "https://script.google.com/macros/s/AKfycbylwS4uf_4lrOgpmmRi3qh8SASnLVwMGIxXe-yO3X1A0mwn2J9psLJheTty0ojumvJ1/exec";
